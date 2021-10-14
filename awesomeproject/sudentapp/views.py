42ad0c27739d0396a0449debd00b051a3da08b91
che <che-email@yandex.ru>
2021-10-14 21:20:47 +0500

    fix

--- awesomeproject/sudentapp/views.py
+++ awesomeproject/sudentapp/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template,RequestContext
 from datetime import datetime as dt
+
 def index(request):
-    template = Template("<h1> Hello {{bar}} </h1>")
-    context = RequestContext(request)
-    context['bar'] = dt.now()
-    return HttpResponse(template.render(context))
+    return render(request,'foo.html',{'bar':dt.now})
